a2916cfa4bed17635a3e9c1e6b6738bf19769938
Ishani Pigera <ishani@surge.global>
2023-09-01 03:14:15 +0530

    feat: speech recognition and voice synthesis

---
+++ main.py
@@ -0,0 +1,44 @@
+import pyttsx3
+import speech_recognition as sr
+
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# Initialize the text-to-speech engine
+engine = pyttsx3.init()
+engine.setProperty("rate", 150)
+
+# Listen for voice command
+def listen_for_command():
+    with sr.Microphone() as source:
+        print("Listening for command...")
+        engine.say("Listening for command...")
+        engine.runAndWait()
+        audio = r.listen(source)
+
+    try:
+        command = r.recognize_google(audio)
+        print("Command:", command)
+        return command.lower()
+    except sr.UnknownValueError:
+        print("Unable to recognize speech")
+        engine.say("Unable to recognize speech")
+        engine.runAndWait()
+        return None
+
+# Main program loop
+while True:
+    command = listen_for_command()
+    if command:
+        if command == "scan":
+            engine.say("Starting environment scan")
+            engine.runAndWait()
+        elif command.startswith("get"):
+            object_label = command.split()[1]
+            engine.say(f"Searching for object: {object_label}")
+            engine.runAndWait()
+        else:
+            engine.say("Invalid command.")
+            engine.runAndWait()
+    else:
+        print("No command detected.")
